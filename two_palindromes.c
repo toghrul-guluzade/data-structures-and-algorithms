53e0bd7160b0e85d86d92cab3a08bf1d5bd6b8cb
toghrul-guluzade <tguluzade19619@ada.edu.az>
2026-01-30 11:49:52 +0400

    some updates

---
+++ two_palindromes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MOD 1000000007
+
+
+
+long long  powmod(long long x, long long n, long long m){
+    if(n == 0)  return 1;
+
+    if(n % 2 == 0) return powmod((x*x) % m, n/2, m);
+    return (x * powmod(x, n-1, m)) % m;
+
+}
+
+
+int main() {
+  
+  long long n, k;
+  scanf("%lld %lld", &n, &k);
+
+  long long res = n * (powmod(k-1, n, MOD)) % MOD;
+
+  printf("%lld", res);
+  return 0;
+}
