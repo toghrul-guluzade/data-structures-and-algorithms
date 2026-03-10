39786d85658810244d10d07783551e0f0ccead0c
toghrul-guluzade <tguluzade19619@ada.edu.az>
2026-03-10 12:36:47 +0400

    exam prep

---
+++ exam_prep/simple_stack.cpp
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <stack>
+
+
+
+
+int main(){
+
+    std::stack<int> s;
+    std::string cmd;
+    int n;
+
+    while(std::cin>>cmd){
+        if(cmd == "push"){
+            std::cin >> n;
+            s.push(n);
+            std::cout << "ok";
+            std::cout << '\n';
+        } else if(cmd ==  "pop"){
+            std::cout << s.top();
+            std::cout << '\n';
+            s.pop();
+        } else if(cmd == "back"){
+            std::cout << s.top();
+            std::cout << '\n';
+        } else if(cmd == "size"){
+            std::cout << s.size();
+            std::cout << '\n';
+        } else if(cmd == "clear"){
+            while(s.size()){
+                s.pop();
+            }
+            std::cout << "ok";
+            std::cout << '\n';
+        } else if(cmd == "exit"){
+            std::cout << "bye";
+            return 0;
+        }
+    }
+
+
+    return 0;
+}
